cc796ddf02890f6e2c68ac3453b4d9d8b6f29629
Mykyta Kozhemiakin <mykyta.kozhemiakin@gmail.com>
2026-03-29 12:22:58 +0200

    Updated project deps

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SimpleReactLightbox from 'simple-react-lightbox'
-
+import process from "process";
+import SimpleReactLightbox from "simple-react-lightbox";
 import App from "./App";
-
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
-ReactDOM.render(<SimpleReactLightbox><App /></SimpleReactLightbox>, document.getElementById("root"));
+window.process = process;
+
+ReactDOM.render(
+  <SimpleReactLightbox>
+    <App />
+  </SimpleReactLightbox>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
